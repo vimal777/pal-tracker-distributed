2db4238bf21b1a0c7f0bc07e20f7fd21fb26e43a
vimal777 <vimal.sawant@capgemini.com>
2017-09-12 12:06:48 +0530

    project client file modify

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -3,18 +3,32 @@ package io.pivotal.pal.tracker.allocations;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
 public class ProjectClient {
 
+
+    private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
     private final RestOperations restOperations;
-    private final String registrationServerEndpoint;
+    private final String endpoint;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations= restOperations;
-        this.registrationServerEndpoint = registrationServerEndpoint;
+        this.restOperations = restOperations;
+        this.endpoint = registrationServerEndpoint;
     }
 
     @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+
+        projectsCache.put(projectId, project);
+
+        return project;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return projectsCache.get(projectId);
     }
 }
--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,8 +1,6 @@
 package io.pivotal.pal.tracker.backlog;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
 
 import java.util.Map;
@@ -10,7 +8,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
     private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
     private final RestOperations restOperations;
     private final String endpoint;
@@ -30,7 +27,6 @@ public class ProjectClient {
     }
 
     public ProjectInfo getProjectFromCache(long projectId) {
-        logger.info("Getting project with id {} from cache", projectId);
         return projectsCache.get(projectId);
     }
 }
