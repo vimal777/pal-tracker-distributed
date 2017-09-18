b6296c4c235e8dc1349b33515e8f902f3d9e383b
vimal777 <vimal.sawant@capgemini.com>
2017-09-18 23:00:02 +0530

    gone

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestOperations;
 import java.util.TimeZone;
 
 
+@EnableWebSecurity
 @EnableResourceServer
 @EnableCircuitBreaker
 @EnableEurekaClient
@@ -28,8 +29,8 @@ public class App {
 
     @Bean
     ProjectClient projectClient(
-        RestOperations restOperations,
-        @Value("${registration.server.endpoint}") String registrationEndpoint
+            RestOperations restOperations,
+            @Value("${registration.server.endpoint}") String registrationEndpoint
     ) {
         return new ProjectClient(restOperations, registrationEndpoint);
     }
